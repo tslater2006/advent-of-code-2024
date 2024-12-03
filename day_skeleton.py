6fc8c29318d819ace40c32d10388636bd6fcb3de
Tim Slater <tslater2006@gmail.com>
2024-12-03 08:04:25 -0600

    Ignore pychace/idea folders

---
+++ day_skeleton.py
@@ -0,0 +1,15 @@
+from base_day import BaseDay
+
+
+class Day2(BaseDay):
+    def __init__(self):
+        super().__init__(1)
+
+    def part1(self) -> str:
+        return "Not implemented"
+
+    def part2(self) -> str:
+        return "Not implemented"
+
+
+Day2().run()
